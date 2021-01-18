4f0fa64be3aa62beed96b4b68efe58b848a6dfa1
jessegj <jen18055@byui.edu>
2021-01-18 14:04:07 -0700

    test php file

--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
-<?php
+<main>
+        <figure>
+        <img src="images/lego-mansion-1.jpg" alt="Lego Mansion">
+        <figcaption>This is my current lego project</figcaption>
+        <button class="button" id="learn" onclick="learn()">Click to find out more</button>
+        </figure>
+        <p>Hello, my name is Jesse. Welcome to my page! I have only three semesters left after this one and I am excited to graduate. What I love to do is design and build my own Lego sets. I did not start doing it on a large scale until about 2012 - 2013. I design them through software that I have and then connect online and find the parts I need to make my project. It is a blast and allows me to build and design a little at a time.</p>    
+    </main>
 
-  phpinfo();
+<?php
 
 ?>
