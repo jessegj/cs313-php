17a5aa0f01c24d34651c82f8e0710eab4c28935d
jessegj <jen18055@byui.edu>
2021-01-18 14:09:01 -0700

    debugging index.php

--- web/index.php
+++ web/index.php
@@ -8,35 +8,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Balthazar&display=swap" rel="stylesheet">
     <title>Homepage CSE341</title>
 </head>
-<body>
-
-    <header>
-        <h1>CSE 341</h1>
-    </header>
-
-    <nav>
-        <div class="navbar">
-            <a href="index.php">Home</a>
-            <a href="assignments.php">Assignments</a>
-        </div>
-    </nav>
-
-    <main>
-        <figure>
-        <img src="images/lego-mansion-1.jpg" alt="Lego Mansion">
-        <figcaption>This is my current lego project</figcaption>
-        <button class="button" id="learn" onclick="learn()">Click to find out more</button>
-        </figure>
-        <p>Hello, my name is Jesse. Welcome to my page! I have only three semesters left after this one and I am excited to graduate. What I love to do is design and build my own Lego sets. I did not start doing it on a large scale until about 2012 - 2013. I design them through software that I have and then connect online and find the parts I need to make my project. It is a blast and allows me to build and design a little at a time.</p>    
-    </main>
-    
-    <hr>
-    
-    <footer>Created by Jesse Jensen CSE341 BYU-Idaho</footer>
-    <? php include 'homepage.php';?>
-    <script src="homepage.js"></script>
-</body>
-</html>
 
 <?php
 
